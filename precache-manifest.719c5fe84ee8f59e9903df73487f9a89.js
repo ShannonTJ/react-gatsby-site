7282c9914b93752283fcf90604069cf5dc9df686
Shannon TJ <shannontj@shaw.ca>
2020-07-02 09:15:43 -0700

    Updates

---
+++ precache-manifest.719c5fe84ee8f59e9903df73487f9a89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5693ca6925c2ad4369c73191d338f508",
+    "url": "/friends-app/index.html"
+  },
+  {
+    "revision": "5368cc26e15ccbbd09f0",
+    "url": "/friends-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "09960e8e68e75667371c",
+    "url": "/friends-app/static/css/main.b52f9e6a.chunk.css"
+  },
+  {
+    "revision": "5368cc26e15ccbbd09f0",
+    "url": "/friends-app/static/js/2.94d83d99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friends-app/static/js/2.94d83d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09960e8e68e75667371c",
+    "url": "/friends-app/static/js/main.5a9c3df8.chunk.js"
+  },
+  {
+    "revision": "b93c2de778580c5514ff",
+    "url": "/friends-app/static/js/runtime-main.cb68757a.js"
+  }
+]);
