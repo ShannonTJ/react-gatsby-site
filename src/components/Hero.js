96cf3ebe84687504a88f35d96c1c49bdea39dd23
Shannon TJ <shannontj@shaw.ca>
2021-02-12 14:10:59 -0700

    hero section styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,9 @@ const Hero = () => {
         <HeroItems>
           <HeroH1>Unreal Destinations</HeroH1>
           <HeroP>Out of this world</HeroP>
-          <Button to="/">Learn More</Button>
+          <Button primary="true" big="true" round="true" to="/trips">
+            Learn More
+          </Button>
         </HeroItems>
       </HeroContent>
     </HeroContainer>
@@ -34,14 +36,53 @@ const HeroContainer = styled.div`
   color: #fff;
 `
 
-const HeroBg = styled.div``
+const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
 
-const VideoBg = styled.video``
+const VideoBg = styled.video`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+`
 
-const HeroContent = styled.div``
+const HeroContent = styled.div`
+  z-index: 3;
+  height: calc(100vh - 80px);
+  max-height: 100%;
+  padding: 0rem calc((100vw - 1300px) / 2);
+`
 
-const HeroItems = styled.div``
+const HeroItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0;
+  color: #fff;
+  line-height: 1.1;
+  font-weight: bold;
+`
 
-const HeroH1 = styled.h1``
+const HeroH1 = styled.h1`
+  font-size: clamp(1.5rem, 6vw, 4rem);
+  margin-bottom: 1.5rem;
+  letter-spacing: 3px;
+  padding: 0 1rem;
+`
 
-const HeroP = styled.p``
+const HeroP = styled.p`
+  font-size: clamp(1rem, 3vw, 3rem);
+  margin-bottom: 2rem;
+`
