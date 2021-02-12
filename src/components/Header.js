c325d37d6b6c6ffb51fc39737eda5bb11e8915fd
Shannon TJ <shannontj@shaw.ca>
2021-02-11 17:55:50 -0700

    add global style

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,5 +39,10 @@ const Bars = styled(FaBars)`
   @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
   }
 `
