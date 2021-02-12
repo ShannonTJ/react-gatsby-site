ac77f673bad1e9637392c6941f40d9472a32f19b
Shannon TJ <shannontj@shaw.ca>
2021-02-12 15:04:35 -0700

    image assets

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -15,8 +15,15 @@ export default Trips
 export const ProductsContainer = styled.div`
   min-height: 100vh;
   padding: 5rem calc((100vw-1300px) / 2);
+  background: #000;
+  color: gray;
 `
 
-export const ProductsHeading = styled.div``
+export const ProductsHeading = styled.div`
+  font-size: clamp(1.2rem, 5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+  color: #000;
+`
 
 export const ProductWrapper = styled.div``
