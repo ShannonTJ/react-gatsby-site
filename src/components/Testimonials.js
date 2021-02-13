f74c57464e184faf7eb3433552624a7c873c5b84
Shannon TJ <shannontj@shaw.ca>
2021-02-13 15:23:05 -0700

    testimonial styling

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -32,7 +32,9 @@ const Testimonials = () => {
             </p>
           </Testimonial>
         </ColumnOne>
-        <ColumnTwo>image</ColumnTwo>
+        <ColumnTwo>
+          <Images />
+        </ColumnTwo>
       </ContentWrapper>
     </TestimonialsContainer>
   )
@@ -40,4 +42,70 @@ const Testimonials = () => {
 
 export default Testimonials
 
-const TestimonialsContainer = styled.div``
+const TestimonialsContainer = styled.div`
+  width: 100%;
+  background: #fcfcfc;
+  color: #000;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  height: 100%;
+`
+
+const Heading = styled.p`
+  color: #077bf1;
+  font-size: 1rem;
+  padding-left: 2rem;
+  margin-bottom: 0.75rem;
+`
+
+const Description = styled.p`
+  text-align: start;
+  padding-left: 2rem;
+  margin-bottom: 4rem;
+  font-size: clamp(1.5rem, 5vw, 2rem);
+  font-weight: bold;
+`
+
+const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const ColumnOne = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+const Testimonial = styled.div`
+  padding-top: 1rem;
+  padding-right: 2rem;
+
+  h3 {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+    font-style: italic;
+  }
+
+  p {
+      color #3b3b3b
+  }
+`
+
+const ColumnTwo = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 2rem;
+  grid-gap: 10px;
+
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
+`
+const Images = styled(Img)`
+  border-radius: 10px;
+  height: 100%;
+`
