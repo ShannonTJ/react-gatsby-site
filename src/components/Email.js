dbf854b3302616a307965cae55db41db75b1b803
Shannon TJ <shannontj@shaw.ca>
2021-02-14 16:54:00 -0700

    email background

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,12 +1,31 @@
 import React from "react"
 import styled from "styled-components"
+import EmailBg from "../assets/images/email-1.jpg"
+import { Button } from "./Button"
 
 const Email = () => {
   return (
-    <div>
-      <h1>Email</h1>
-    </div>
+    <EmailContainer>
+      <EmailContent>
+        <h1>Get Access to Exclusive Offers</h1>
+        <p>Sign up for our newsletter to get $100 off your first trip</p>
+        <form action="#">
+          <FormWrap>
+            <label htmlFor="email">
+              <input type="email" placeholder="Enter your email" id="email" />
+            </label>
+            <Button>Sign Up</Button>
+          </FormWrap>
+        </form>
+      </EmailContent>
+    </EmailContainer>
   )
 }
 
 export default Email
+
+const EmailContainer = styled.div``
+
+const EmailContent = styled.div``
+
+const FormWrap = styled.div``
