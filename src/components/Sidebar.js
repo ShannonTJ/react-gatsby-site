8256e3b1b96bce7ca5ec76664410999bb111dbf8
Shannon TJ <shannontj@shaw.ca>
2021-02-15 15:09:20 -0700

    sidebar container styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,6 +16,17 @@ export default Sidebar
 
 const SidebarContainer = styled.aside`
   position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: #0d0d0d;
+  display: grid;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0%")};
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
 `
 
 const Icon = styled.div``
