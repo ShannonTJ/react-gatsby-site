cb6847e40a7c7e751e628974b798707b1dabed7b
Shannon TJ <shannontj@shaw.ca>
2021-02-15 15:37:30 -0700

    sidebar styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from "react"
 import styled from "styled-components"
+import { Link } from "gatsby"
 import { FaTimes } from "react-icons/fa"
+import { Button } from "./Button"
 
 const Sidebar = () => {
   return (
@@ -15,6 +17,18 @@ const Sidebar = () => {
           <SidebarLink to="/">Careers</SidebarLink>
           <SidebarLink to="/">Contact</SidebarLink>
         </SidebarMenu>
+        <SideBtnWrap>
+          <Button
+            to="/"
+            primary="true"
+            round="true"
+            css={`
+              font-size: 1.2rem;
+            `}
+          >
+            Book a Flight
+          </Button>
+        </SideBtnWrap>
       </SidebarWrapper>
     </SidebarContainer>
   )
@@ -50,4 +64,40 @@ const Icon = styled.div`
 
 const CloseIcon = styled(FaTimes)`
   color: #fff;
+`
+
+const SidebarWrapper = styled.div`
+  color: #fff;
+`
+
+const SidebarMenu = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(5, 80px);
+  text-align: center;
+
+  @media screen and (max-width: 480px) {
+    grid-template-rows: repeat(5, 60px);
+  }
+`
+
+const SidebarLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    color: #f26a2e;
+    transition: 0.2s ease-in-out;
+  }
+`
+const SideBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
 `
